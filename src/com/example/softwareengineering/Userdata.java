c430e8c922713157c82629740b0d8fa4da08fd02
kreamganatanatda <kimhyorin.khr@gmail.com>
2016-10-29 09:40:47 +0700

    Update Userdata.class

--- src/com/example/softwareengineering/Userdata.java
+++ src/com/example/softwareengineering/Userdata.java
@@ -1,40 +1,54 @@
 package com.example.softwareengineering;
 
 public class Userdata {
-    private String arrlogin;
+    private String uLogin;
     private String arrpassword;
     private String arrresource;
     private String arrrole;
+    private String uDate_start;
+    private String uDate_end;
+    private String uVolume;
 
-    public Userdata(String arrlogin, String arrpassword, String arrresource, String arrrole) {
-        this.arrlogin = arrlogin;
+    public Userdata(String uLogin, String arrpassword, String arrresource, String arrrole,
+                    String uDate_start, String uDate_end, String uVolume) {
+        this.uLogin = uLogin;
         this.arrpassword = arrpassword;
         this.arrresource = arrresource;
         this.arrrole = arrrole;
+        this.uDate_start = uDate_start;
+        this.uDate_end = uDate_end;
+        this.uVolume = uVolume;
     }
 
     public Userdata() {
-        this.arrlogin = null;
+        this.uLogin = null;
         this.arrpassword = null;
         this.arrresource = null;
         this.arrrole = null;
+        this.uDate_start = null;
+        this.uDate_end = null;
+        this.uVolume = null;
     }
 
     public boolean isEmpty() {
-        return ((arrlogin == null) && (arrrole == null) && (arrpassword == null) && (arrresource == null));
+        return ((uLogin == null) && (arrrole == null) && (arrpassword == null) && (arrresource == null));
     }
 
     public boolean authentication() {
-        return ((arrlogin != null) && (arrpassword != null));
+        return ((uLogin != null) && (arrpassword != null));
     }
 
     public boolean authorization() {
         return (authentication() && (arrrole != null) && (arrresource != null));
     }
 
+    public boolean accounting() {
+        return (authorization() && (uDate_start != null) && (uDate_end != null) && (uVolume != null));
+    }
+
 
     public void setLogin(String login) {
-        this.arrlogin = login;
+        this.uLogin = login;
     }
 
     public void setPassword(String password) {
@@ -50,7 +64,7 @@ public class Userdata {
     }
 
     public String getLogin() {
-        return arrlogin;
+        return uLogin;
     }
 
     public String getPassword() {
@@ -65,4 +79,27 @@ public class Userdata {
         return arrrole;
     }
 
+    public String getuDate_start() {
+        return uDate_start;
+    }
+
+    public void setuDate_start(String uDate_start) {
+        this.uDate_start = uDate_start;
+    }
+
+    public String getuDate_end() {
+        return uDate_end;
+    }
+
+    public void setuDate_end(String uDate_end) {
+        this.uDate_end = uDate_end;
+    }
+
+    public String getuVolume() {
+        return uVolume;
+    }
+
+    public void setuVolume(String uVolume) {
+        this.uVolume = uVolume;
+    }
 }
